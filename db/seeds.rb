3c8970d01aba456df753c7cd226aef116b988aff
Oscar Bernardini <bernardini687@gmail.com>
2019-03-08 13:18:31 +0100

    Modify seeds for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Setting up...'
 
-require 'json'
-require 'open-uri'
+# require 'json'
+# require 'open-uri'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients = JSON.parse(open(url).read)
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients = JSON.parse(open(url).read)
 
-limit = ingredients['drinks'].length
+# limit = ingredients['drinks'].length
 
 puts 'Cleaning database...'
 Ingredient.destroy_all
 
-12.times do
-  random_number = (0..limit).to_a.sample
-  ingredient = ingredients['drinks'][random_number]
+# 12.times do
+#   random_number = (0..limit).to_a.sample
+#   ingredient = ingredients['drinks'][random_number]
 
-  ingredient = ingredient.values[0] unless ingredient.nil?
-  Ingredient.create!(name: ingredient)
-end
+#   ingredient = ingredient.values[0] unless ingredient.nil?
+#   Ingredient.create!(name: ingredient)
+# end
+
+Ingredient.create(name: 'Lemon')
+Ingredient.create(name: 'Banana')
+Ingredient.create(name: 'Ananas')
+
+Ingredient.create(name: 'Kiwi')
+Ingredient.create(name: 'Spinach')
+Ingredient.create(name: 'Broccoli')
+
+Ingredient.create(name: 'Mango')
+Ingredient.create(name: 'Orange')
+Ingredient.create(name: 'Carrot')
+
+Ingredient.create(name: 'Tomato')
+Ingredient.create(name: 'Strawberry')
+Ingredient.create(name: 'Cherry')
 
 puts 'Finished!'
